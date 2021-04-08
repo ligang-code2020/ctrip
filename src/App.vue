b62b618524612e0e68faa235e2e06347afd69c7d
ligang <ligang13874843373@gmail.com>
2021-04-08 20:53:39 +0800

    feat: init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 顶部搜索 -->
+    <div class="search-index">
+      <div class="search">搜索:目的地/酒店/景点/航班号</div>
+      <a href="#" class="user">我 的</a>
+    </div>
+    <!-- 焦点图模块 -->
+    <div class="focus">
+      <img src="../src/upload/focus.jpg" alt="" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
+<style  src="../src/css/ctrip.css"></style>
+<style  src="../src/css/normalize.css"></style>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* color: #2c3e50; */
+  margin: 0 auto;
 }
 </style>
