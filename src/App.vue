4f97d3f171767361ec56fd17646565eab32e2f6e
ligang <ligang13874843373@gmail.com>
2021-04-14 21:28:12 +0800

    feat：新增local-nav

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <!-- 顶部搜索 -->
     <div class="search-index">
       <div class="search">搜索:目的地/酒店/景点/航班号</div>
@@ -7,8 +7,25 @@
     </div>
     <!-- 焦点图模块 -->
     <div class="focus">
-      <img src="../src/upload/focus.jpg" alt="" />
+      <!-- <img src="../src/upload/focus.jpg" alt="" /> -->
+      <div class="block">
+        <span class="demonstration"></span>
+        <el-carousel height="150px" indicator-position="none">
+          <el-carousel-item v-for="(item, index) in img_list" :key="index">
+            <img :src="item" alt="" />
+          </el-carousel-item>
+        </el-carousel>
+      </div>
     </div>
+    <!-- 局部导航栏 -->
+    <ul class="local-nav">
+      <li v-for="(item, index) in local_nav" :key="index">
+        <a href="#" :title="item.name">
+          <span class="local-nav-icon" :style='`"${item.style}"`'></span>
+          <span>{{ item.name }}</span>
+        </a>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -16,18 +33,56 @@
 export default {
   name: "App",
   components: {},
+  data() {
+    return {
+      img_list: [
+        require("../src/upload/focus.jpg"),
+        require("../src/upload/pic3.jpg"),
+        require("../src/upload/focus.jpg"),
+      ],
+      local_nav: [
+        {
+          name: "景点游玩",
+          img: require("../src/assets/localnav_bg.png"),
+          style:"{background: 'url(' + ../src/assets/localnav_bg.png + ') no-repeat 0 0',backgroundSize:'32px auto'}"
+        },
+        {
+          name: "周边游",
+          img: require("../src/assets/localnav_bg.png"),
+        },
+        {
+          name: "美食林",
+        },
+        {
+          name: "一日游",
+        },
+        {
+          name: "当地攻略",
+        },
+      ],
+    };
+  },
 };
 </script>
 
-<style  src="../src/css/ctrip.css"></style>
-<style  src="../src/css/normalize.css"></style>
+<style src="../src/css/ctrip.css"></style>
+<style src="../src/css/normalize.css"></style>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  /* color: #2c3e50; */
+/* body{
   margin: 0 auto;
+} */
+
+.el-carousel__item h3 {
+  color: #475669;
+  font-size: 14px;
+  opacity: 0.75;
+  line-height: 0;
+  margin: 0;
+}
+.el-carousel__arrow--right {
+  display: none;
+}
+.el-carousel__arrow--left {
+  display: none;
 }
 </style>
